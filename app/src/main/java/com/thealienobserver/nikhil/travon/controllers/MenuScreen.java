39dff9ab9653a8aa95aae7038e63ba0e8c57f55d
Genny Centeno <gn936133@dal.ca>
2018-10-27 11:10:49 -0300

    Added onClick on recommended places

--- app/src/main/java/com/thealienobserver/nikhil/travon/controllers/MenuScreen.java
+++ app/src/main/java/com/thealienobserver/nikhil/travon/controllers/MenuScreen.java
@@ -1,10 +1,14 @@
 package com.thealienobserver.nikhil.travon.controllers;
 
+import android.content.Intent;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
@@ -28,6 +32,8 @@ public class MenuScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu_screen);
+
+
 
         mLatitude = getIntent().getStringExtra(LATITUDE);
         mLatitude = getIntent().getStringExtra(LATITUDE);
@@ -71,6 +77,14 @@ public class MenuScreen extends AppCompatActivity {
                 places.release();
             }
         });
+    }
+
+    public void openRecommendedPlaces(View view) {
+        Toast.makeText(getApplicationContext(), "This is my Toast message!", Toast.LENGTH_LONG).show();
+
+        //  Intent recommendedPlacesIntent = new Intent(MenuScreen.this, RecommendedPlacesActivity.class);
+      //  startActivity(recommendedPlacesIntent);
+
     }
 
 }
